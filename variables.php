5f42193f522626bee882990351572515dc41bc16
RitwikCalidig <88304830+Ritwik799@users.noreply.github.com>
2021-08-18 07:03:53 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$name = 'Ritwik';
+$number = 100;
+$numberList = 100.5;
+
+echo $name . $number; //Concatenation
+echo $name . " " . $number; //Concatenation
+
+$name = "<h1> HELLO </h1>";
+echo $name;
+
+?>
+
+</body>
+</html>
